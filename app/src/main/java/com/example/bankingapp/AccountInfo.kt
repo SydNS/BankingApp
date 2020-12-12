aef5373ac448cf120ae731f9b60f7e44ba9307fc
SydNS <sydneynsiimbe@gmail.com>
2020-12-12 14:13:32 +0300

    added a fragment for display in the account info for a given person

---
+++ app/src/main/java/com/example/bankingapp/AccountInfo.kt
@@ -0,0 +1,22 @@
+package com.example.bankingapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class AccountInfo : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v:View = inflater.inflate(R.layout.fragment_accountinfo, container, false)
+        val tv: TextView =v.findViewById(R.id.tvResult5)
+        tv.text="tftyjvytg"
+        return v
+    }
+
+}
