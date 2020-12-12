aa3a3e036c97f5eee583fbb12babf0e435d61ae7
SydNS <sydneynsiimbe@gmail.com>
2020-12-12 13:48:43 +0300

    added the Transactions.kt fragment to list the transactions made

--- app/src/main/java/com/example/bankingapp/Transactions.kt
+++ app/src/main/java/com/example/bankingapp/Transactions.kt
@@ -1,7 +1,26 @@
 package com.example.bankingapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 
 class Transactions: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        @Nullable container: ViewGroup?,
+        @Nullable savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val v: View = inflater.inflate(R.layout.transactions, container, false)
+        val tv: TextView =v.findViewById(R.id.tvResult5)
+        tv.text="tftyjvytg"
+        return v
+
+
+    }
 
 }
